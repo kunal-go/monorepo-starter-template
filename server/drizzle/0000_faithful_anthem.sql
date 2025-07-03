071e01353c95a2c4bc1a0f60cddc7661d8814ff0
Kunal Gosrani <kunalgosrani10@gmail.com>
2025-07-04 03:26:22 +0530

    chore: add drizzle files

---
+++ server/drizzle/0000_faithful_anthem.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "reset_password_requests" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"new_password_hash" text NOT NULL,
+	"otp_hash" text NOT NULL,
+	"valid_till" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "user_sessions" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"valid_till" timestamp NOT NULL,
+	"refresh_key" text,
+	"refreshed_at" timestamp
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"email" text NOT NULL,
+	"hashed_password" text NOT NULL,
+	"password_updated_at" timestamp,
+	"is_verified" boolean DEFAULT false NOT NULL,
+	"is_insider" boolean DEFAULT false NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "verification_requests" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"user_id" uuid NOT NULL,
+	"hashed_otp" text NOT NULL,
+	"valid_till" timestamp NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "reset_password_requests" ADD CONSTRAINT "reset_password_requests_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_sessions" ADD CONSTRAINT "user_sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "verification_requests" ADD CONSTRAINT "verification_requests_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
